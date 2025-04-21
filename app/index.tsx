fee04276950535eda7984e7438ec7735e735594b
David Lennox <dmlennox03@gmail.com>
2025-04-21 14:36:43 +1000

    basic data querying from firestore

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,9 @@ import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE } from 'react-native-maps'
 import { auth } from '../FirebaseConfig'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { useRouter } from 'expo-router'
+import { collection, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { db } from '../FirebaseConfig'; //db instance from firebase config.
 
 const INITAL_REGION = {
   latitude: -37.8136,
@@ -12,12 +15,26 @@ const INITAL_REGION = {
   longitudeDelta: 0.0421,
 };
 
+//EXPORT APP
 export default function App() {
   const router = useRouter();
+  const [places, setPlaces] = React.useState([]);
+
+  const retrievePlaces = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "places"));
+      const placesData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })); //mapping data to object array
+      console.log("Places retrieved from the firestore DB:", placesData);
+    } catch (error) {
+      console.error("Error fetching places:", error);
+    }
+  };
+  
   return (
       <View style={{ flex: 1 }}>
           <View style={{ position: 'absolute', top: 50, left: 20, zIndex: 1 }}>
               <Button title="(testing) Go to Login" onPress={() => router.push('/login')} />
+              <Button title="(testing) Retrieve from firestore" onPress={retrievePlaces} />
           </View>
           <MapView style={StyleSheet.absoluteFill} provider={PROVIDER_DEFAULT} initialRegion={INITAL_REGION} />
       </View>
