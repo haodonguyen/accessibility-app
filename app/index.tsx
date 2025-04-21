b7f6d2d4d60e16b6891736ade463463237ec4552
David Lennox <dmlennox03@gmail.com>
2025-04-21 15:16:22 +1000

    retrieved places are now shown on the map

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native'
-import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 import { auth } from '../FirebaseConfig'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { useRouter } from 'expo-router'
@@ -15,17 +15,49 @@ const INITAL_REGION = {
   longitudeDelta: 0.0421,
 };
 
+type Place = {
+  id: string;
+  place_name: string;
+  place_description: string;
+  place_phonenumber: number;
+  place_address: string;
+  place_coordinates: {
+    latitude: any;
+    longitude: any;
+  };
+  place_owner_id: string;
+  place_wheelchair_accessible: boolean;
+}
+
 //EXPORT APP
 export default function App() {
   const router = useRouter();
-  const [places, setPlaces] = React.useState([]);
+  const [places, setPlaces] = useState<Place[]>([]);
 
   const retrievePlaces = async () => {
     try {
       const querySnapshot = await getDocs(collection(db, "places"));
-      const placesData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })); //mapping data to object array
-      console.log("Places retrieved from the firestore DB:", placesData);
+      const placesData: Place[] = querySnapshot.docs.map(doc => {
+        const data = doc.data();
+        return { //same mapping as before but properly typed for react usestate
+          //note: the values after '||' are default values if the associated data is missing from firestore.
+          id: doc.id,
+          place_name: data.place_name || "Missing name",
+          place_description: data.place_description || "missing description",
+          place_phonenumber: data.place_phonenumber || "0000000000",
+          place_address: data.place_address || "missing address",
+          place_coordinates: {
+            latitude: data.place_coordinates.latitude,
+            longitude: data.place_coordinates.longitude,
+          },
+          place_owner_id: data.place_owner_id || "missing owner id",
+          place_wheelchair_accessible: data.place_wheelchair_accessible || false,
+        };
+      });
+      console.log("Places retrieved from the Firestore DB:", placesData);
+      setPlaces(placesData);
     } catch (error) {
+      alert("Failed to retrieve places.");
       console.error("Error fetching places:", error);
     }
   };
@@ -36,7 +68,22 @@ export default function App() {
               <Button title="(testing) Go to Login" onPress={() => router.push('/login')} />
               <Button title="(testing) Retrieve from firestore" onPress={retrievePlaces} />
           </View>
-          <MapView style={StyleSheet.absoluteFill} provider={PROVIDER_DEFAULT} initialRegion={INITAL_REGION} />
+        
+          <MapView style={StyleSheet.absoluteFill} provider={PROVIDER_DEFAULT} initialRegion={INITAL_REGION}>
+            {places.map(place => (
+
+              <Marker //place markers with the data from firestore
+                key={place.id}
+                coordinate={{
+                  latitude: place.place_coordinates.latitude,
+                  longitude: place.place_coordinates.longitude,
+                }}
+                title={place.place_name}
+                description={place.place_description}
+
+              />
+            ))}
+          </MapView>
       </View>
   );
 }
