c304409361b2832df0ed1285dd949bf92393d919
David Lennox <dmlennox03@gmail.com>
2025-04-27 14:55:24 +1000

    added more disability-relevant info to our data model

--- components/submitPlaceMenu.tsx
+++ components/submitPlaceMenu.tsx
@@ -1,5 +1,5 @@
 import React, { Component, useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, TextInput } from 'react-native'
+import { Text, View, StyleSheet, Button, TextInput, Switch } from 'react-native'
 import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 import { auth } from '../FirebaseConfig'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, signOut } from 'firebase/auth'
@@ -19,6 +19,11 @@ export default function SubmitPlaceMenu({ place_coordinates, onClose }: Props) {
     const [placePhoneNumber, setPlacePhoneNumber] = useState('')
     const [placeAddress, setPlaceAddress] = useState('')
     const [placeWheelchair, setPlaceWheelchair] = useState(false) //example accessibility information
+    const [wheelchairDescription, setWheelchairDescription] = useState('')
+    const [placeBlind, setPlaceBlind] = useState(false)
+    const [blindDescription, setBlindDescription] = useState('')
+    const [placeAuditory, setPlaceAuditory] = useState(false)
+    const [auditoryDescription, setAuditoryDescription] = useState('')
 
     const dbInsert = async () => {
         try {
@@ -29,9 +34,21 @@ export default function SubmitPlaceMenu({ place_coordinates, onClose }: Props) {
                 place_address: placeAddress,
                 place_coordinates: place_coordinates, //value comes from passed prop
                 place_original_submitter_id: auth.currentUser?.uid, //get the current user id from firebaseauth
-                place_owner_id: null, //this should be which account owns the venue and can edit it. if null, anybody shall be able to edit it.
-                place_wheelchair_accessible: placeWheelchair,
-                place_submission_timestamp: new Date().toISOString()
+                place_owner_id: null, //this should be which business or account owns the venue and can edit it. if null, anybody shall be able to edit it.
+                place_submission_timestamp: new Date().toISOString(),
+
+                //accessibility:
+                place_wheelchair_accessible: placeWheelchair, //does this place have access for wheelchairs and limited mobility?
+                place_wheelchair_description: wheelchairDescription, //if yes, how? elevator? ramp?
+                place_wheelchair_rating: null, //1-5 rating of wheelchair access. this is null upon submission, since rating is done by community and users later
+
+                place_blind_accessible: placeBlind, //is this place accessible for blind & visually impaired?
+                place_blind_description: blindDescription,
+                place_blind_rating: null,
+
+                place_auditory_accessible: placeAuditory, //is this place accessible for deaf & hearing impaired?
+                place_auditory_description: auditoryDescription, 
+                place_auditory_rating: null,
             });
             console.log("DB Insertion complete with ID: ", docRef.id);
             onClose(); // Close the modal after successful submission
@@ -47,7 +64,7 @@ export default function SubmitPlaceMenu({ place_coordinates, onClose }: Props) {
             <TextInput placeholder='Place description' value={placeDescription} onChangeText={setPlaceDescription} />
             <TextInput placeholder='Place phone number' value={placePhoneNumber} onChangeText={setPlacePhoneNumber} keyboardType='numeric' />
             <TextInput placeholder='Place address' value={placeAddress} onChangeText={setPlaceAddress} />
-            <Text>coords: {place_coordinates.latitude}, {place_coordinates.longitude}</Text>
+            <Text>(debug) coords: {place_coordinates.latitude}, {place_coordinates.longitude}</Text>
             <Button title='Submit' onPress={dbInsert} />
         </View>
     );
