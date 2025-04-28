d4de8c171aedc40d962c69a8054d13397af8065a
David Lennox <dmlennox03@gmail.com>
2025-04-28 22:00:54 +1000

    dbinsertion 2 and scrollview

--- app/submitPlace.tsx
+++ app/submitPlace.tsx
@@ -1,5 +1,5 @@
 import React, { Component, useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, TextInput, Switch} from 'react-native'
+import { Text, View, StyleSheet, Button, TextInput, Switch, ScrollView} from 'react-native'
 import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 import { auth, db, googlemapsAPIKey } from '../FirebaseConfig'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, signOut } from 'firebase/auth'
@@ -16,6 +16,7 @@ export default function submitPlace() {
   const [placeDescription, setPlaceDescription] = useState('')
   const [placePhoneNumber, setPlacePhoneNumber] = useState('')
   const [placeAddress, setPlaceAddress] = useState('')
+  const [place_coordinates, setPlaceCoordinates] = useState({ latitude: 0, longitude: 0 });
   const [placeWheelchair, setPlaceWheelchair] = useState(false) //example accessibility information
   const [wheelchairDescription, setWheelchairDescription] = useState('')
   const [placeBlind, setPlaceBlind] = useState(false)
@@ -23,14 +24,40 @@ export default function submitPlace() {
   const [placeAuditory, setPlaceAuditory] = useState(false)
   const [auditoryDescription, setAuditoryDescription] = useState('')
 
+const dbInsert = async () => {
+        try {
+            const docRef = await addDoc(collection(db, "places"), {
+                place_name: placeName,
+                place_description: placeDescription,
+                place_phonenumber: placePhoneNumber,
+                place_address: placeAddress,
+                place_coordinates: place_coordinates,
+                place_original_submitter_id: auth.currentUser?.uid, //get the current user id from firebaseauth
+                place_owner_id: null, //this should be which business or account owns the venue and can edit it. if null, anybody shall be able to edit it.
+                place_submission_timestamp: new Date().toISOString(),
+
+                //accessibility:
+                place_wheelchair_accessible: placeWheelchair, //does this place have access for wheelchairs and limited mobility?
+                place_wheelchair_description: wheelchairDescription, //if yes, how? elevator? ramp?
+
+                place_blind_accessible: placeBlind, //is this place accessible for blind & visually impaired?
+                place_blind_description: blindDescription,
+
+                place_auditory_accessible: placeAuditory, //is this place accessible for deaf & hearing impaired?
+                place_auditory_description: auditoryDescription, 
+            });
+            console.log("DB Insertion complete with ID: ", docRef.id);
+        } catch (error:any) {
+            console.error("DB Error: " + error);
+        }
+    }
 
   return (
-    <View>
+    <ScrollView style={{ flex: 1 }}>
       <Text>Submit a place</Text>
-      <TextInput placeholder='Place name' value={placeName} onChangeText={setPlaceName} />
-      <TextInput placeholder='Place description' value={placeDescription} onChangeText={setPlaceDescription} />
-      <TextInput placeholder='Place phone number' value={placePhoneNumber} onChangeText={setPlacePhoneNumber} keyboardType='numeric' />
-      <TextInput placeholder='Place address' value={placeAddress} onChangeText={setPlaceAddress} />
+      <TextInput placeholder='Enter the venue name' value={placeName} onChangeText={setPlaceName} />
+      <TextInput placeholder='Enter a description' value={placeDescription} onChangeText={setPlaceDescription} />
+      <TextInput placeholder='Venue phone number' value={placePhoneNumber} onChangeText={setPlacePhoneNumber} keyboardType='numeric' />
 
       <Text>Disability information</Text>
       <AccessibilityInput
@@ -59,8 +86,9 @@ export default function submitPlace() {
         onDescriptionChange={setAuditoryDescription}
         placeholder="What is hearing impairment access like at this place?"
         backgroundColor="lightgreen"
-      />            
-            
-    </View>
+      />
+
+      <Button title='Submit' onPress={dbInsert} />
+    </ScrollView>
   );
 }
